22faa87b645da70b249772efe61952a572a00026
amogomoses12 <amogomoses10@gmail.com>
2024-04-13 00:55:11 +0100

    added js file

---
+++ switcher.js
@@ -0,0 +1,15 @@
+/*   ----------theme colors----------   */
+const alternateStyles = document.querySelectorAll(".alternate-style");
+function setActiveStyle(color)
+{
+    alternateStyles.forEach((style) => {
+        if(color === style.getAttribute("title"))
+        {
+            style.removeAttribute("disabled");
+        }
+        else
+        {
+            style.setAttribute("disabled", "true");
+        }
+    })
+}
